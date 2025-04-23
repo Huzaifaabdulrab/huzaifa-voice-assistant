acff48bc9cce0eac6350dceecf38628fe4daeafd
Huzaifaabdulrab <huzaifaabdulrab2@gmail.com>
2025-04-23 15:58:09 +0500

    voice assistent

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="huzaifa-voice-assistant",
-    version="0.1.0",
+    version="0.2.0",
     author="Huzaifa Abdulrab",
     description="Voice assistant for visually impaired users using speech recognition.",
     long_description=open("README.md", encoding="utf-8").read(),
