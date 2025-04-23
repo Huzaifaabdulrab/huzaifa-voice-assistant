5a21d6657d6ea17a4fa86b3f2fadce17fe9d7b83
Huzaifaabdulrab <huzaifaabdulrab2@gmail.com>
2025-04-24 02:30:36 +0500

    voice assistant

--- build/lib/huzaifa_voice_assistant/__init__.py
+++
@@ -1,6 +0,0 @@
-# huzaifa_voice_assistant/__init__.py
-
-from .main import run_assistant
-
-# Run assistant automatically when imported
-run_assistant()
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="huzaifa-voice-assistant",
-    version="0.0.2",  # Increase this if re-uploading
+    version="0.2.1",  # Increase this if re-uploading
     author="Huzaifa Abdulrab",
     description="Voice assistant for visually impaired users using speech recognition.",
     long_description=open("README.md", encoding="utf-8").read(),
@@ -21,4 +21,9 @@ setup(
         "pyaudio",
         "pywhatkit"
     ],
+    entry_points={
+        'console_scripts': [
+            'huzaifa-voice-assistant=huzaifa_voice_assistant.assistant:run_assistant',
+        ],
+    },
 )
